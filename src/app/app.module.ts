e4bd77705ba1957f4d938ea9ef36201088d07096
Juan Jose Soriano <escobar@carteldamageclaims.com>
2020-06-01 21:15:37 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DndUploadComponent } from './components/addons/dnd-upload/dnd-upload.component';
+import { FileUploadComponent } from './components/addons/file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndUploadComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
---
+++ src/app/components/addons/dnd-upload/dnd-upload.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-dnd-upload',
+  templateUrl: './dnd-upload.component.html',
+  styleUrls: ['./dnd-upload.component.scss']
+})
+export class DndUploadComponent implements OnInit {
+  @Output() private selectedFile: EventEmitter<File> = new EventEmitter();
+  public currentFile: File = null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onFileChange(file: File) {
+    console.log('currentfile', file);
+    if (this.fileValidation(file)) {
+      this.currentFile = file;
+      this.selectedFile.emit(file);
+      return;
+    }
+
+    console.error('Error');
+  }
+
+  public removeFile() {
+    this.currentFile = null;
+    this.selectedFile.emit(null);
+  }
+
+  public fileValidation(file: File) {
+    let isExcelFile = file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+    return isExcelFile;
+  }
+}
